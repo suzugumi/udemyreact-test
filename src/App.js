92188812d2f213f43b274990cffd8c9f4889632e
“suzugumi” <limcatd2@gmail.com>
2019-10-24 12:14:42 +0000

    jsx練習

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div><h1>hoge</h1></div>
-  );
+// function App() {
+//   return (
+//     <div><h1>hoge</h1></div>
+//   );
+// }
+
+class App extends Component {
+  render(){
+    return React.createElement(
+      "h1",
+      null,
+      "hello, Word!"
+    );
+  }
 }
 
 export default App;
