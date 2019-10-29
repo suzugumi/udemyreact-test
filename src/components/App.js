39f640285d89afe3eb277fd8271c69ca1cebd7b3
“suzugumi” <limcatd2@gmail.com>
2019-10-29 13:33:46 +0000

    udemy react講座セクション5 チャプター31

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,41 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-const App = () => (<Counter></Counter>)
+// const App = () => (<Counter></Counter>)
 
-class Counter extends Component {
-  constructor(props){
-    super(props)
-    this.state = { count: 0}
-  }
+// class Counter extends Component {
+//   constructor(props){
+//     super(props)
+//     this.state = { count: 0}
+//   }
 
-  handlePlusButton = () => {
-    console.log("handlePlusButton");
-    this.setState({ count: this.state.count + 1})
-  }
+//   handlePlusButton = () => {
+//     console.log("handlePlusButton");
+//     this.setState({ count: this.state.count + 1})
+//   }
 
-  handlemainButton = () => {
-    this.setState({ count: this.state.count -1 })
+//   handlemainButton = () => {
+//     this.setState({ count: this.state.count -1 })
 
-    console.log("handlemainButton");
-    if(0 >= this.state.count){
-      console.log('うんち')
-      this.setState({ count:  0 })
+//     console.log("handlemainButton");
+//     if(0 >= this.state.count){
+//       console.log('うんち')
+//       this.setState({ count:  0 })
 
-    }
+//     }
 
-  }
+//   }
 
-  render(){
-  return ( 
-  <React.Fragment>
-  <div>count: { this.state.count }</div>
-  <button onClick={ this.handlePlusButton}>+1</button>
-  <button onClick={ this.handlemainButton}>-1</button>
-  </React.Fragment>
-  )
-  }
-}
+//   render(){
+//   return ( 
+//   <React.Fragment>
+//   <div>count: { this.state.count }</div>
+//   <button onClick={ this.handlePlusButton}>+1</button>
+//   <button onClick={ this.handlemainButton}>-1</button>
+//   </React.Fragment>
+//   )
+//   }
+// }
 
 
-export default App;
+// export default App;
 
