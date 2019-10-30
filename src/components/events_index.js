a13db9c63d86af41e009fb1041764f394ad95923
“suzugumi” <limcatd2@gmail.com>
2019-10-30 11:58:06 +0000

    udemy react勉強5-33

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash'
+import { Link } from 'react-router-dom';
 import { readEvents } from '../actions';
 
 
@@ -19,8 +20,9 @@ class EventsIndex extends Component {
     ))
   }
   render(){
-    const props = this.props
+    // const props = this.props
   return ( 
+    <React.Fragment>
     <table>
       <thead>
         <tr>
@@ -33,6 +35,8 @@ class EventsIndex extends Component {
         {this.renderEvents()}
       </tbody>
     </table>
+    <Link to="/events/new">New Event</Link>
+    </React.Fragment>
    )
   }
 }
