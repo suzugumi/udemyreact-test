bc821fb3a583efc38298242d5c83645796c66fd7
“suzugumi” <limcatd2@gmail.com>
2019-11-02 10:18:20 +0000

    udemy react 5-40

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,11 +1,20 @@
 import _ from 'lodash'
 import { 
   READ_EVENTS,
+  CREATE_EVENT,
+  READ_EVENT,
+  UPDATE_EVENT,
   DELETE_EVENT,
  } from '../actions'
 
 export default (events = {}, action) => {
   switch (action.type) {
+    case CREATE_EVENT:
+    case READ_EVENT:
+    case UPDATE_EVENT:
+      console.log(action.response.data)
+      const data = action.response.data
+      return { ...events, [data.id]:data }
     case READ_EVENTS:
       return _.mapKeys(action.response.data, 'id')
       case DELETE_EVENT:
